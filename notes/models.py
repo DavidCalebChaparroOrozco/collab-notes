6f32c81572c26be71bdd31aace82409ebd9e33d6
dcaleb98 <vladictor1998@gmail.com>
2026-08-12 21:50:24 -0500

    Feat: expands the title to 200 characters according to revision

--- notes/models.py
+++ notes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Note(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
