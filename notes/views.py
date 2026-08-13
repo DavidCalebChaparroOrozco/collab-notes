92436d6ef92a77440910128b06b3b4bfa6b74c6a
dcaleb98 <vladictor1998@gmail.com>
2026-08-12 22:31:42 -0500

    feat: Add a personalized greeting to the start screen.

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.https import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1> My Notes App </h1> <p> Your space to think, write and share your ideas. </p> ")
