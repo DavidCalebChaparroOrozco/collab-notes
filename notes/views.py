255ea7f11185832f7ca43d00b58f26150ff81a79
dcaleb98 <vladictor1998@gmail.com>
2026-08-13 03:14:33 +0000

    Feat: Adds a homepage with a greeting

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1> Collab Notes </h1> <p> Welcome to the Notes App! </p>")
