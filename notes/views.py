f9cf37a7788c22d0066d8c6dfc9f63e234f19549
dcaleb98 <vladictor1998@gmail.com>
2026-08-19 02:35:21 -0500

    feat: adds footer

--- notes/views.py
+++ notes/views.py
@@ -2,4 +2,4 @@ from django.https import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Collab Notes </h1> <p> Your space to think, write and share your ideas.</p> ")
+    return HttpResponse("<h1> Collab Notes </h1> <p> Your space to think, write and share your ideas. </p> <footer> Collab Notes 226 </footer> ")
